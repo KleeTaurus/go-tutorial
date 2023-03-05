9890cb246ecd616a45478b569190ccfcefae3612
Kevin Lee <klee.taurus@gmail.com>
2023-03-05 13:17:00 +0800

    implement binary search in go

---
+++ algorithms/binary_search.go
@@ -0,0 +1,25 @@
+package main
+
+func search(nums []int, target int) int {
+	if len(nums) == 0 {
+		return -1
+	}
+
+	low := 0
+	high := len(nums) - 1
+
+	for low <= high {
+		mid := (low + high) / 2
+		if target == nums[mid] {
+			return mid
+		}
+
+		if target < nums[mid] {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+
+	return -1
+}
