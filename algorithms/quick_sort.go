22b8bd53105db77c0d982cabb4931399704fca4b
Kevin Lee <klee.taurus@gmail.com>
2023-03-09 23:15:11 +0800

    code refactor

--- algorithms/quick_sort.go
+++ algorithms/quick_sort.go
@@ -5,23 +5,21 @@ func quickSort(nums []int) []int {
 		return nums
 	}
 
-	mid := nums[len(nums)/2]
+	pivot := nums[len(nums)/2]
 	lower := []int{}
 	higher := []int{}
 	for i, x := range nums {
 		if i == len(nums)/2 {
 			continue
 		}
-		if x <= mid {
+		if x <= pivot {
 			lower = append(lower, x)
 		} else {
 			higher = append(higher, x)
 		}
 	}
 
-	target := []int{}
-	target = append(target, quickSort(lower)...)
-	target = append(target, mid)
+	target := append(quickSort(lower), pivot)
 	target = append(target, quickSort(higher)...)
 	return target
 }
